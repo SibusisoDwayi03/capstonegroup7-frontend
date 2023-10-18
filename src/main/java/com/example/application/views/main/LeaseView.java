f1ca2cb8488c961d224c1d3f10359f5bd0086a2b
ZEDAYa1 <220097429@mycput.ac.za>
2023-10-18 23:21:08 +0200

    Created a frontend for lease.

---
+++ src/main/java/com/example/application/views/main/LeaseView.java
@@ -0,0 +1,9 @@
+package com.example.application.views.main;
+
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+
+public class LeaseView extends VerticalLayout {
+
+}
